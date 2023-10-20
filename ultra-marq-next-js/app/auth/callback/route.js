5383e1d6bac55cc988c047403748ad07fbdcc3bc
Michael Mehr <michaelmehr0@gmail.com>
2023-10-19 22:11:48 -0500

    auth/callback

--- ultra-marq-next-js/app/auth/callback/route.js
+++ ultra-marq-next-js/app/auth/callback/route.js
@@ -3,15 +3,14 @@ import { cookies } from 'next/headers'
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-    const reqUrl = new URL(req.url)
-    const code = reqUrl.get('code')
+    const cookieStore = cookies()
+    const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+    const { searchParams } = new URL(req.url)
+    const code = searchParams.get('code')
     
     if (code) {
-        const cookieStore = cookies()
-        const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
         await supabase.auth.exchangeCodeForSession(code)
     }
 
-    // URL to redirect to after the sign in process completes
-    return NextResponse.redirect(reqUrl.origin)
+    return NextResponse.redirect(new URL('/account', req.url))
 }
