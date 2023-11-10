08268b2676be4bad3eb72d8d60a44f5f832362eb
Michael Mehr <michaelmehr0@gmail.com>
2023-11-09 18:31:06 -0600

    Users able to delete bookmarks, refresh on delete

--- app/components/bookmark.jsx
+++ app/components/bookmark.jsx
@@ -1,10 +1,33 @@
+'use client'
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Bookmark({ bookmark }) {
+  const router = useRouter()
+  async function deleteBookmark() {
+    const supabase = createClientComponentClient()
+    try {
+      const { error } = await supabase
+        .from('bookmarks')
+        .delete()
+        .eq('id', bookmark.id)
+      if (error) {
+        throw error
+      }
+      alert("Bookmark deleted")
+    } catch (error) {
+      alert("ERROR: Could not delete bookmark")
+    } finally {
+      router.refresh()
+    }
+  }
+
   return (
     <li key={bookmark.id}>
       <Link href={bookmark.url}>{bookmark.title}</Link>
-      <button>X</button>
+      <button onClick={deleteBookmark}>X</button>
     </li>
   )
 }
