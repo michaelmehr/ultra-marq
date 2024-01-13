3f2077c3457906a58d6e8ad40d6fc6edc1aa3f2b
Michael Mehr <michaelmehr0@gmail.com>
2024-01-12 18:04:49 -0600

    updated styling for dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -11,9 +11,11 @@ export default async function Dashboard() {
   } = await supabase.auth.getSession();
 
   return (
-    <div>
+    <div className="w-4/5 m-auto">
       <h1 className="text-5xl font-bold">Dashboard</h1>
-      <UserBookmarks session={session} />
+      <div className="py-4">
+        <UserBookmarks session={session} />
+      </div>
     </div>
   );
 }
